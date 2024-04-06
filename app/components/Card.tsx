da48c1f8e54ea6fc1864e31900cc6999fd8a2f19
radeau <radeau@adso-rad>
2024-04-06 09:51:30 +0400

    Build: update style

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -10,24 +10,30 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ href, cardTitle, cardMessage }) => {
     return (
-      <div className="max-w-sm m-6 h-full bg-white border border-gray-200 rounded-xl shadow-xl dark:bg-gray-800 dark:border-gray-700">
-        <a href={`/projects` + href} target="_blank">
-            {/* <Image src={blogComponent} alt="blog preview card component" className="w-full h-64 rounded-l" unoptimized /> */}
-            <div className="w-full h-64 rounded-t-l bg-gradient-to-r from-green-400 to-blue-500 flex justify-center items-center">
-                <div className="w-80 h-52 bg-white rounded-xl flex justify-center items-center font-extrabold text-2xl">
-                    <h1 className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">{cardTitle}</h1>
+      <div className="max-w-sm my-6 sm:m-6 h-full w-full bg-white border border-gray-200 rounded-xl shadow-xl dark:bg-gray-800 dark:border-gray-700">
+        <div className="">
+            {/* href={`/projects` + href} // for production*/} 
+            <a href={`/projects` + href} target="_blank">
+                {/* <Image src={blogComponent} alt="blog preview card component" className="w-full h-64 rounded-l" unoptimized /> */}
+                <div className="w-full h-56 rounded-t-xl bg-gradient-to-r from-green-400 to-blue-500 flex justify-center items-center">
+                    <div className="w-80 h-16 py-20 px-14 bg-white rounded-xl flex justify-center items-center font-extrabold text-2xl">
+                        <h1 className="mx-2 text-center bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">{cardTitle}</h1>
+                    </div>
                 </div>
-            </div>
-        </a>
-        <div className="p-5">
+            </a>
+        </div>
+        <div className="h-40 p-5">
             {/* <a href="#">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{cardTitle}</h5>
             </a> */}
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{cardMessage}</p>
-            <a href={href} target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        </div>
+        <div className="flex justify-center my-6">
+            {/* href={`/projects` + href} // for production*/} 
+            <a href={`/projects` + href} target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Live Here
             </a>
-            <a href="https://github.com/radeau" target="_blank" className="inline-flex items-center mx-4 px-3 border rounded-lg py-2 text-sm font-medium text-center text-blue-700">
+            <a href="https://github.com/radeau/frontend-mentor-challenges" target="_blank" className="inline-flex items-center mx-4 px-3 border rounded-lg py-2 text-sm font-medium text-center text-blue-700">
                 Github
             </a>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,8 @@ export default function BlogPreview() {
   <main className="flex min-h-screen flex-col items-center justify-between p-5">
     <div className="flex flex-col sm:flex-row sm:mx-3 justify-evenly">
       <Card href={'/nft-preview-card'} cardTitle={'NFT Card Component'} cardMessage={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'} />
-      <Card href={'/blog-preview-card'} cardTitle={'Blog Preview Card'} cardMessage={'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sun.'} />
-      <Card href={'/order-summary-page'} cardTitle={'Order Summary Page'} cardMessage={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'} />
-    </div>
-    <div className="flex flex-col sm:flex-row sm:mx-3 justify-evenly">
-      <Card href={'/nft-preview-card'} cardTitle={'The Third Title'} cardMessage={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'} />
-      <Card href={'/blog-preview-card'} cardTitle={'The Eighty Eight Title'} cardMessage={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'} />
-      <Card href={'/order-summary-page'} cardTitle={'The Hundred Title'} cardMessage={'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sun.'} />
-    </div>
-    <div className="flex flex-col sm:flex-row sm:mx-3 justify-evenly">
-      <Card href={'/nft-preview-card'} cardTitle={'NFT Card Component'} cardMessage={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'} />
-      <Card href={'/blog-preview-card'} cardTitle={'Blog Preview Card'} cardMessage={'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sun.'} />
-      <Card href={'/order-summary-page'} cardTitle={'Order Summary Page'} cardMessage={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'} />
+      <Card href={'/blog-preview-card'} cardTitle={'Blog Preview Card'} cardMessage={'At vero eoiditate non provident, similique sun.'} />
+      <Card href={'/order-summary-component'} cardTitle={'Order Summary Component'} cardMessage={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'} />
     </div>
   </main>
   );
